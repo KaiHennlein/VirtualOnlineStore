a72a1eae3cd72b727593e502e7a6f1ffd1b33b88
KaiHennlein <64425440+KaiHennlein@users.noreply.github.com>
2023-11-09 14:29:28 +0100

    Added resource folder for JSON files

---
+++ Assets/Scripts/ResourceLoaders/JSONList.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class JSONList
+{
+
+    //Saves a list of JSONs to use them for product descriptions and assign the correct image to the product
+}
